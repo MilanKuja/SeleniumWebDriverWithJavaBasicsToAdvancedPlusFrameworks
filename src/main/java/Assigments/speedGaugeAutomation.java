8d657020dac45b8cce04798475dfb8bb10df2ca6
MilanKuja <ilmilan95@gmail.com>
2026-07-01 15:46:09 -0500

    renamed

--- src/main/java/Assigments/speedGaugeAutomation.java
+++ src/main/java/Assigments/speedGaugeAutomation.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class slackWeightStationDriverName {
+public class speedGaugeAutomation {
     public static void main(String[] args) throws InterruptedException {
 
 
